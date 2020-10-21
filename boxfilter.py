96a76d6cf9456b88b92852ce210b7d36a5241fba
GustavoBMendes <mendes.gustavob@gmail.com>
2020-10-20 21:44:34 -0300

    finalizado

--- boxfilter.py
+++ boxfilter.py
@@ -14,22 +14,20 @@ print(arquivos)
 
 imagem = cv2.imread(arquivos[0], 0)
 cv2.imshow('Imagem original', imagem)
+cv2.imwrite('escaladecinza.png',imagem)
 
 proporcao = int(arquivos[1])
 
 print("Altura: %d pixels" % (imagem.shape[0]))
 print("Largura: %d pixels" % (imagem.shape[1]))
 
-print(imagem)
 
-#reduzida = np.array(imagem[0:int(imagem.shape[0]/proporcao), 0:int(imagem.shape[1]/proporcao)], dtype=np.int32)
-#aux = np.arange(0, int(imagem.shape[0]), proporcao)
-
-#reduzida[:, ::1] = aux
 i = 0
 j = 0
 colunas = []
 linhas = []
+
+#Percorrendo a matriz original para cálculo das médias
 while i < imagem.shape[0]:
 	while j < imagem.shape[1]:
 		media = imagem[i:proporcao+i, j:proporcao+j]
@@ -42,22 +40,28 @@ while i < imagem.shape[0]:
 	j = 0
 
 reduzida = np.array(linhas, dtype=np.uint8)
-print(reduzida)
 print("Altura: %d pixels" % (reduzida.shape[0]))
 print("Largura: %d pixels" % (reduzida.shape[1]))
 
+#downsampling com box filter
 cv2.imshow("Imagem reduzida com box filter", reduzida)
+cv2.imwrite('reduzida_bf.png',reduzida)
+
+#upsampling do box filter
 upBF = np.repeat(reduzida, proporcao, axis=1)
 upBF = np.repeat(upBF, proporcao, axis=0)
 cv2.imshow('Box filter no tamanho original', upBF)
+cv2.imwrite('up_bf.png',upBF)
 
-#redução por downsampling
+#redução por downsampling fatiamento
 downsampling = imagem[::proporcao, ::proporcao]
-cv2.imshow('Reduzida por vizinho mais proximo', downsampling)
+cv2.imshow('Reduzida por downsampling simples', downsampling)
+cv2.imwrite('reduzida_fatiamento.png',downsampling)
 
 #retorno ao tamanho original através de upsampling
 upsampling = np.repeat(downsampling, proporcao, axis=1)
 upsampling = np.repeat(upsampling, proporcao, axis=0)
-cv2.imshow('Upsampling vizinho mais proximo', upsampling)
+cv2.imshow('Upsampling fatiamento simples', upsampling)
+cv2.imwrite('up_fatiamento.png',upsampling)
 
 cv2.waitKey(0)
